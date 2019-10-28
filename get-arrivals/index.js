39d7a99b1bff726a3d001ec73a738c7de0905e29
Chen Ye <cye@alumni.brown.edu>
2019-10-28 01:59:21 -0700

    passthru

--- get-arrivals/index.js
+++ get-arrivals/index.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -9,7 +11,9 @@ module.exports = async function (context, req) {
         } catch (error) {
             context.res = {
                 status: 500,
+                body: error.toString(),
             }
+            console.error(error);
         }
     }
     else {
@@ -23,14 +27,10 @@ module.exports = async function (context, req) {
 const OTP_BASE_URL = 'https://otp.metroservices.io/otp';
 
 const getOTPStopTimes = async (stopId) => {
-    // return {};
-    const request = new Request(`${OTP_BASE_URL}/routers/default/index/stops/${stopId}/stoptimes/`, {
-        body: new URLSearchParams([
-            ['omitNonPickups', 'true'],
-            ['numberOfDepartures', '2'],
-        ])
-    })
-    const response = await fetch(request);
+    const response = await fetch(`${OTP_BASE_URL}/routers/default/index/stops/${stopId}/stoptimes/?${new URLSearchParams([
+        ['omitNonPickups', 'true'],
+        ['numberOfDepartures', '2'],
+    ])}`);
     if (!response.ok) {
         throw new Error(response.statusText);
     }
